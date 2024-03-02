2501184cc9279b9afe7c88902121e0a2874d3ff3
Michael Mayer <michael.mayer@maibornwolff.de>
2024-03-02 21:54:25 +0100

    fix animation

--- src/components/Curtain.tsx
+++ src/components/Curtain.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 
 const Curtain = () => {
   const [isLoaded, setIsLoaded] = useState(false);
-  const [lineAnimation, setLineAnimation] = useState(false);
+  const [lineAnimation, setLineAnimation] = useState<boolean>(false);
 
   useEffect(() => {
     // Simulate a 2-second delay
@@ -21,16 +21,19 @@ const Curtain = () => {
     }; // Cleanup the timeouts on component unmount
   }, []);
 
+  const scale = !lineAnimation ? "scale-y-0" : "scale-y-100"
+
   return (
     <>
+      (
       <div
-        className={`fixed left-0 top-0 z-10 h-full w-full transform bg-black transition-transform duration-1000 ${isLoaded ? '-translate-x-full' : '-translate-x-1/2'}`}
+        className={`fixed left-0 top-0 z-10 h-full w-full transform bg-black transition-transform duration-500 ${isLoaded ? '-translate-x-full' : '-translate-x-1/2'}`}
       />
       <div
-        className={`fixed left-1/2 top-0 h-full w-0.5 origin-center transform bg-white ${isLoaded ? 'invisible' : 'visible'} transition-transform duration-1000 scale-y-${lineAnimation ? '100' : '0'} z-20`}
+        className={`fixed left-1/2 top-0 h-full w-0.5 origin-center transform bg-white ${isLoaded ? 'invisible' : 'visible'} transition-transform duration-700 ${scale} z-20`}
       />
       <div
-        className={`fixed left-0 top-0 z-10 h-full w-full transform bg-black transition-transform duration-1000 ${isLoaded ? 'translate-x-full' : 'translate-x-1/2'}`}
+        className={`fixed left-0 top-0 z-10 h-full w-full transform bg-black transition-transform duration-500 ${isLoaded ? 'translate-x-full' : 'translate-x-1/2'}`}
       />
     </>
   );
