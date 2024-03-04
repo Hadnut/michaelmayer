f3e9adc840da6cb395e08c34e10b22493e19ab29
Michael Mayer <michael.mayer@maibornwolff.de>
2024-03-04 21:25:31 +0100

    add icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,14 @@
+import { FaLinkedin } from 'react-icons/fa';
+import { IoMdMail } from 'react-icons/io';
+
 export default function Footer() {
   return (
     <footer className="flex h-16 items-center justify-between bg-white text-black">
-      {/* Logo */}
-      <h1 className="w-full text-lg">Copyright © 2024 | Michael Mayer</h1>
-      {/* Desktop Navigation */}
+      <h1 className="w-full text-lg">Copyright © 2024</h1>
+      <div className="md:flex">
+        <FaLinkedin />
+        <IoMdMail />
+      </div>
     </footer>
   );
 }
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -4,13 +4,6 @@ export default function Contact() {
       <span className="mb-12 inline-block font-bold uppercase tracking-[8px] text-[#333]">
         Get in Touch
       </span>
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2663.244569137237!2d11.549931176659635!3d48.12480675204978!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479ddf4e5a3a28ed%3A0xfb905ab23b89360!2sSchmellerstra%C3%9Fe%2011%2C%2080337%20M%C3%BCnchen!5e0!3m2!1sde!2sde!4v1709392310571!5m2!1sde!2sde"
-        width="400"
-        height="300"
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
-      ></iframe>
       <div className="mt-28 flex justify-between">
         <div className="md:w-[45%]">
           <span className="mb-12 inline-block font-bold uppercase tracking-[8px] text-[#333]">
